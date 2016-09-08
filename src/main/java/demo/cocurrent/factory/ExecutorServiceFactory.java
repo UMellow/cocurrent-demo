8223216bc2b61e231cd5b8d87f1d987040b68b46
UMellow <304159633@qq.com>
2016-09-08 17:33:00 +0800

    完善代码

--- src/main/java/demo/cocurrent/factory/ExecutorServiceFactory.java
+++ src/main/java/demo/cocurrent/factory/ExecutorServiceFactory.java
@@ -1,6 +1,7 @@
 package demo.cocurrent.factory;
 
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -17,32 +18,63 @@ public class ExecutorServiceFactory {
 	 */
 	private ExecutorServiceFactory() {
 		
-	}
+	}	
 	
 	/**
 	 * 提供获取当前对象实例的getInstance方法
 	 * 
 	 * @return ExecutorServiceFactory 当前类实例（Singleton） 
 	 */
-	public ExecutorServiceFactory getInstance() {
+	public static ExecutorServiceFactory getInstance() {
 		
 		return factory;
 	}
 	
-	private ThreadFactory getThreadFactory() {
+	/**
+	 * 创建一个可安排的线程池
+	 * 
+	 * @return ExecutorService 
+	 * 			线程执行器，可安排在给定延迟后运行命令或者定期地执行
+	 */
+	public ExecutorService createSchedulerThreadPool() {
 		
-		AtomicInteger atomicInteger = new AtomicInteger();
+		int processors = Runtime.getRuntime().availableProcessors();
+		executorService = Executors.newScheduledThreadPool(processors * 10, getThreadFactory());
+		return executorService;
+	}
+	
+	/**
+	 * 创建一个可重用固定线程数的线程池
+	 * 
+	 * @return ExecutorService
+	 * 			线程执行器，以共享的无界队列方式来运行这些线程 
+	 */
+	public ExecutorService createFixedThreadPool(int nThreads) {
+		
+		executorService = Executors.newFixedThreadPool(nThreads, getThreadFactory());
+		return executorService;
+	}
+	
+	private ThreadFactory getThreadFactory() {			
 		
 		return new ThreadFactory() {
 			
 			@Override
 			public Thread newThread(Runnable r) {
 				
-				/*public Thread newThread(Runnable runnable) {
-					
-				}*/
+				AtomicInteger atomicInteger = new AtomicInteger();
+				SecurityManager securityManager = System.getSecurityManager();
 				
-				return null;
+				ThreadGroup threadGroup;
+				if(securityManager != null) {
+					threadGroup = securityManager.getThreadGroup();
+				} else {
+					threadGroup = Thread.currentThread().getThreadGroup();
+				}
+				
+				Thread thread = new Thread(threadGroup, r);
+				thread.setName("任务线程 - " + atomicInteger.incrementAndGet());
+				return thread;											
 			}
 		};
 	}
